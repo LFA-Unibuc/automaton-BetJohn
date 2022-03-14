f96a14452c45ae5808f0e4e20dbaa43fbe7ed641
BestJohn <Ioan_rey_misterio@yahoo.com>
2022-03-15 00:52:57 +0200

    Tema automat validare date

--- README.md
+++ README.md
@@ -1,36 +1,32 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-f059dc9a6f8d3a56e377f745f24479a46679e63a5d9fe6f495e02850cd0d8118.svg)](https://classroom.github.com/online_ide?assignment_repo_id=7137750&assignment_repo_type=AssignmentRepo)
+
 # automaton
+
 ### First LFA Lab
+
 Exercise 1. Implement a library/program in a programming language of your
 choosing to load and validate the following input file:
+
 ```
-#
-# comment lines ( skip them )
-#
-# This is a comment
 Sigma :
-    word1
-    word2
-    ...
+    a
+    b
 End
-#
-# comment lines ( skip them )
-#
+
 States :
-    state1
-    state2
-    state3 ,F
-    ...
-    stateK , S
-    ...
+    1
+    2
+    3 ,F
+    5 ,S
 End
-#
-# comment lines ( skip them )
-#
+
 Transitions :
-    stateX, wordY , stateZ
-    stateX, wordY , stateZ
-    ...
+    1, a , 2
+    1, b , 3
+    2, a , 2
+    2, b , 3
+    3, a , 1
+    5, b , 2
 End
 ```
 
--- automaton.py
+++ automaton.py
@@ -21,14 +21,81 @@ class Automaton():
         pass
 
     def read_input(self, input_str):
-        """Return the automaton's final configuration
-        
-        If the input is rejected, the method raises a
-        RejectionException.
-        """
-        pass
-    
+        citire = open(input_str, "r")
+
+        Alfabet = set()
+        Stari = set()
+        Tranzitii = []
+        stari_finale = set()
+
+        counter = 0
+        text_input = citire.readlines()
+        for linie in text_input:
+            if linie.startswith('\n') or linie.startswith("#"):
+                # inseamna ca trebuie sa ignor
+                continue
+
+            if "Sigma" in linie:
+                # inseamna ca am intrat in alfabet
+                counter = 1
+                continue
+
+            if 'States' in linie:
+                # inseamna ca am intrat in stari
+                continue
+
+            if 'Transitions' in linie:
+                # inseamna ca am intrat in tranzitii
+                continue
+
+            if linie.startswith('End'):
+                # inseamna ca trec la urmatorul pas
+                counter += 1
+                continue
+
+            if counter == 1:
+                # pun in alfabet litera
+                Alfabet.add(linie.strip())
+
+            if counter == 2:
+                if len(linie.strip().split(', ')) > 2:
+                    Stari.add(linie.strip().split()[0])
+
+                    if linie.strip().split(',')[2] == 'F':
+                        stari_finale.add(linie.strip().split(' ,')[0])
+                    if linei.strip().split(',')[2] == 'S':
+                        initial_state = linei.strip().split(' ,')[0]
+                    if linie.strip().split(',')[1] == 'F':
+                        stari_finale.add(linie.strip().split(' ,')[0])
+                    if linie.strip().split(',')[1] == 'S':
+                        initial_state = linie.strip().split(' ,')[0]
+
+                elif len(linie.strip().split(',')) == 2:
+                    Stari.add(linie.strip().split()[0])
+                    if linie.strip().split(',')[1] == 'S':
+                        initial_state = linie.strip().split(' ,')[0]
+                    if linie.strip().split(',')[1] == 'F':
+                        stari_finale.add(linie.strip().split(' ,')[0])
+                else:
+                    Stari.add(linie.strip())
+            if counter == 3:
+                text = linie.strip().split(", ")
+                for i in range(len(text)):
+                    if i == 1:
+                        text[i] = text[i][:-1]
+                Tranzitii.append(tuple(text))
+        este_ok = 1
+
+        for tranzitie in Tranzitii:
+            # aici verific ca toate elementele din tranzitii sa fie
+            if tranzitie[0] not in Stari or tranzitie[2] not in Stari or tranzitie[1] not in Alfabet:
+                print("Inputul nu este valid")
+                este_ok = 0
+        if este_ok == 1:
+            print("Inputul este valid!")
+        return 1
+
 
 if __name__ == "__main__":
-    a = Automaton('your_config_file')
-    print(a.validate())
+    a = Automaton('date.txt')
+    a.read_input('date.txt')
